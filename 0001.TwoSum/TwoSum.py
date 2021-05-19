df5c6b789502f01cd5d6aa7afc6f416e3c96f8df
fanscen <fanscen@gmail.com>
2021-05-19 14:44:16 +0800

    add 0001TwoSum

---
+++ 0001.TwoSum/TwoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums , target):
+        for i in range(len(nums) - 1):
+            x = nums[i]
+            if target - x in nums:
+                j =  i + 1
+                while j < len(nums) :
+                    y = nums[j]
+                    if target == x + y :
+                        return [i,j]
+                    j = j + 1
+        return []
---
+++ 0001.TwoSum/TwoSumTest.py
@@ -0,0 +1,7 @@
+import TwoSum
+
+mac = TwoSum.Solution()
+
+print(mac.twoSum([2,7,11,15], 9))
+print(mac.twoSum([3,2,4], 6))
+print(mac.twoSum([3,3], 6))
