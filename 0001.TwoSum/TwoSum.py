9ae0f8b55c4c9876606f308d47effc51934cc534
fanscen <fanscen@gmail.com>
2025-05-09 04:52:09 +0000

    none

--- 0001.TwoSum/TwoSum.py
+++ 0001.TwoSum/TwoSum.py
@@ -11,3 +11,18 @@ class Solution:
                     j = j + 1
         return []
 
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums1 = [2, 7, 11, 15]
+    target1 = 9
+    result1 = solution.twoSum(nums1, target1)
+    print(f'Input: nums = {nums1}, target = {target1}, Output: {result1}')
+    nums2 = [3, 2, 4]
+    target2 = 6
+    result2 = solution.twoSum(nums2, target2)
+    print(f'Input: nums = {nums2}, target = {target2}, Output: {result2}')
+    nums3 = [3, 3]
+    target3 = 6
+    result3 = solution.twoSum(nums3, target3)
+    print(f'Input: nums = {nums3}, target = {target3}, Output: {result3}')
