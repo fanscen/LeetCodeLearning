6b9d687a37418a6b12bbe1692021757eda7217c1
fanscen <fanscen@gmail.com>
2025-06-03 21:18:47 +0800

    类型(范围): 简要说明
    说明：
    - 简要说明修改的动机和背景（可选）
    
    变更内容：
    - [ ] 简述修改点1
    - [ ] 简述修改点2
    
    备注：
    - 关联问题：#编号
    - 是否为破坏性变更：否

--- problems/0001.TwoSum/TwoSum.py
+++ problems/0001.TwoSum/TwoSum.py
@@ -1,30 +1,28 @@
 class Solution:
-    def twoSum(self, nums , target):
+    def twoSum(self, nums, target):
         for i in range(len(nums) - 1):
             x = nums[i]
             if target - x in nums:
-                j =  i + 1
-                while j < len(nums) :
+                j = i + 1
+                while j < len(nums):
                     y = nums[j]
-                    if target == x + y :
-                        return [i,j]
+                    if target == x + y:
+                        return [i, j]
                     j = j + 1
         return []
 
 
- 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     solution = Solution()
     nums1 = [2, 7, 11, 15]
     target1 = 9
     result1 = solution.twoSum(nums1, target1)
-    print(f'Input: nums = {nums1}, target = {target1}, Output: {result1}')
+    print(f"Input: nums = {nums1}, target = {target1}, Output: {result1}")
     nums2 = [3, 2, 4]
     target2 = 6
     result2 = solution.twoSum(nums2, target2)
-    print(f'Input: nums = {nums2}, target = {target2}, Output: {result2}')
+    print(f"Input: nums = {nums2}, target = {target2}, Output: {result2}")
     nums3 = [3, 3]
     target3 = 6
-    result3 = solution.twoSum(nums3, target3)
-    print(f'Input: nums = {nums3}, target = {target3}, Output: {result3}')
+    result3 = solution.twoSum(nums3, target1)
+    print(f"Input: nums = {nums3}, target = {target3}, Output: {result3}")
